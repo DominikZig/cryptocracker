cc721ff4377915e913340ee6b877b293a4e4c60b
Dominik Zig <dominikzig@hotmail.co.nz>
2020-01-25 12:01:07 +1300

    Initial commit

---
+++ src/main/java/zigmanov/dominik/cryptocracker/CryptocrackerApplication.java
@@ -0,0 +1,13 @@
+package zigmanov.dominik.cryptocracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocrackerApplication.class, args);
+	}
+
+}
