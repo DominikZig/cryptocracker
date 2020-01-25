d49512b9d98566e2cb33496abf8a75e9484ef781
Dominik Zig <dominikzig@hotmail.co.nz>
2020-01-26 12:55:07 +1300

    Moved functionality to Controller.

--- src/main/java/zigmanov/dominik/cryptocracker/CryptocrackerApplication.java
+++ src/main/java/zigmanov/dominik/cryptocracker/CryptocrackerApplication.java
@@ -2,35 +2,12 @@ package zigmanov.dominik.cryptocracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zigmanov.dominik.cryptocracker.model.CipherText;
-import zigmanov.dominik.cryptocracker.model.Key;
-import zigmanov.dominik.cryptocracker.model.PlainText;
-import zigmanov.dominik.cryptocracker.service.CaesarService;
 
 @SpringBootApplication
 public class CryptocrackerApplication
 {
-	static Key testKey = new Key(3);
-
-	static PlainText testPlainText = new PlainText("abcd");
-
-	static CipherText testCipherText = new CipherText("");
-
 	public static void main(String[] args)
 	{
 		SpringApplication.run(CryptocrackerApplication.class, args);
-
-		CaesarService theService = new CaesarService();
-
-		System.out.println("Initial message: "+ testPlainText.getPlainText());
-
-		testCipherText.setCipherText(theService.encryptMessage(testKey, testPlainText));
-
-		System.out.println("Encrypted message: "+testCipherText.getCipherText());
-
-		testPlainText.setPlainText(theService.decryptMessage(testKey, testCipherText));
-
-		System.out.println("Decrypted message: "+testPlainText.getPlainText());
-
 	}
 }
--- src/main/java/zigmanov/dominik/cryptocracker/controller/IndexController.java
+++ src/main/java/zigmanov/dominik/cryptocracker/controller/IndexController.java
@@ -1,14 +1,42 @@
 package zigmanov.dominik.cryptocracker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import zigmanov.dominik.cryptocracker.model.CipherText;
+import zigmanov.dominik.cryptocracker.model.Key;
+import zigmanov.dominik.cryptocracker.model.PlainText;
+import zigmanov.dominik.cryptocracker.service.CaesarService;
 
 @RestController
 public class IndexController
 {
-        @RequestMapping("/")
-        public String index()
-        {
-            return "Spring Boot Example";
-        }
+    @Autowired
+    private CaesarService theService;
+
+    @GetMapping("/")
+    public String index()
+    {
+        Key testKey = new Key(3);
+
+        PlainText testPlainText = new PlainText("abcd");
+
+        CipherText testCipherText = new CipherText("");
+
+        System.out.println("Initial message: "+ testPlainText.getPlainText());
+
+        testCipherText.setCipherText(theService.encryptMessage(testKey, testPlainText));
+
+        System.out.println("Encrypted message: "+testCipherText.getCipherText());
+
+        testPlainText.setPlainText(theService.decryptMessage(testKey, testCipherText));
+
+        System.out.println("Decrypted message: "+testPlainText.getPlainText());
+
+        return "Initial message: "+ testPlainText.getPlainText()+
+                "Encrypted message: "+testCipherText.getCipherText()+
+                "Decrypted message: "+testPlainText.getPlainText();
+    }
 }
