893c6a47a405c0b14ebf927e7235b073c794c1b0
Dominik Zig <dominikzig@hotmail.co.nz>
2020-01-27 00:30:55 +1300

    Added React frontend.

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import lock from './padlock-10-128.png';
+import unlock from './padlock-9-128.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <img className="lock" src={lock} alt="lock"/>
+          <h1>CryptoCracker</h1>
+          <img className="unlock" src={unlock} alt="unlock"/>
       </header>
     </div>
   );
