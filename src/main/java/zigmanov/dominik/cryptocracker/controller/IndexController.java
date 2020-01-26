081af964f7f36862486a0de16f27d509b3795747
Dominik Zig <dominikzig@hotmail.co.nz>
2020-01-26 14:01:05 +1300

    Added index.html.

--- src/main/java/zigmanov/dominik/cryptocracker/controller/IndexController.java
+++ src/main/java/zigmanov/dominik/cryptocracker/controller/IndexController.java
@@ -2,8 +2,6 @@ package zigmanov.dominik.cryptocracker.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import zigmanov.dominik.cryptocracker.model.CipherText;
 import zigmanov.dominik.cryptocracker.model.Key;
