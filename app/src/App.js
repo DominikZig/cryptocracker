d3dbf06bc04d0581caf904ccc5b08fb704221e7b
Dominik Zig <dominikzig@hotmail.co.nz>
2020-01-28 22:50:20 +1300

    Removed unnecessary code.

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
 import '../node_modules/bulma/css/bulma.css';
 import lock from './padlock-10-128.png';
@@ -6,16 +6,6 @@ import unlock from './padlock-9-128.png';
 import FormInput from "./formInput";
 
 function App() {
-    const [encryptedText, setEncryptedText] = useState("");
-
-    useEffect(() => {
-        fetch('/encrypt')
-            .then(response => response.text())
-            .then(encryptedText => {
-                setEncryptedText(encryptedText);
-            });
-    }, []);
-
     return (
     <div className="App">
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
@@ -27,7 +17,6 @@ function App() {
         <body>
             <div className="centre">
                 <FormInput/>
-                {/*<h1>{encryptedText}</h1>*/}
             </div>
         </body>
     </div>
--- app/src/formInput.js
+++ app/src/formInput.js
@@ -9,8 +9,7 @@ class FormInput extends React.Component {
         this.state = {
             showEncrypt: true,
             showDecrypt: false,
-            plainTextData: "",
-            textValue: "",
+            plainTextValue: "",
             keyValue: undefined,
             encryptedText: ""
         };
@@ -51,7 +50,7 @@ class FormInput extends React.Component {
 
         this.getEncrypted();
 
-        this.state.textValue = this.state.encryptedText;
+        this.state.plainTextValue = this.state.encryptedText;
     };
 
     handleDecryptSubmit = e => {
@@ -66,7 +65,7 @@ class FormInput extends React.Component {
         return (
             <React.Fragment>
             <form onSubmit={this.handleEncryptSubmit}>
-                    <div id={"inputBox"}><input name="textValue" className="input is-large inputBox" type="text" value={this.state.textValue} onChange={this.handleChange} placeholder="Enter your message"/></div>
+                    <div id={"inputBox"}><input name="plainTextValue" className="input is-large inputBox" type="text" value={this.state.plainTextValue} onChange={this.handleChange} placeholder="Enter your message"/></div>
                     <div id={"key"}><input name="keyValue" className="input key" type="text" value={this.state.keyValue} onChange={this.handleChange} placeholder="Key"/></div>
                     <input type="submit" value="Encrypt" className="button is-success" style={{ display : this.state.showEncrypt ? "" : "none"}}/>
             </form>
